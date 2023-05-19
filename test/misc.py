07885f44a32333e61b0b9e474c8fa029c3191e81
Kasliwal17 <kasliwaladitya17@gmail.com>
2023-05-19 10:02:30 +0530

    Update misc.py

--- test/misc.py
+++ test/misc.py
@@ -15,8 +15,8 @@ def get_config(action, action2, config_path=""):
     Get the configuration file as json from it 
     """
     
-    root_path = os.path.dirname(os.path.dirname(
-        os.path.dirname(os.path.realpath(__file__))))
+    root_path = os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__)))
     config_path = os.path.join(root_path, 'configs')
     action = action + '.json'
     with open(os.path.join(config_path, action), encoding='UTF-8') as f1:
