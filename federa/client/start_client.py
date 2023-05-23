fec7fb50fc48db925e25361fc656838c4a84109e
Kasliwal17 <kasliwaladitya17@gmail.com>
2023-05-23 12:03:02 +0530

    Update start_client.py

--- federa/client/start_client.py
+++ federa/client/start_client.py
@@ -4,11 +4,16 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", type=str, default = "localhost:8214", help="IP address of the server")
 parser.add_argument("--device", type=str, default = "cpu", help="Device to run the client on")
+parser.add_argument('--ca', type = str, default= 'ca.pem', help= 'path to CA certificate')
+parser.add_argument('--encryption', type = int, default= 0, help= '1 enables ssl encryption')
+
 args = parser.parse_args()
 
 configs = {
     "ip_address": args.ip,
-    "device": args.device
+    "device": args.device,
+    "encryption": args.encryption,
+    "ca": args.ca,
 }
 
 if __name__ == '__main__':
