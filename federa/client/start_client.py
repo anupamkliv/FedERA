790317d6c55774679dbca1a392fbef5514496eed
Kasliwal17 <kasliwaladitya17@gmail.com>
2023-06-07 08:56:10 +0530

    Update start_client.py
    Adding wait_time to the arguments

--- federa/client/start_client.py
+++ federa/client/start_client.py
@@ -6,12 +6,13 @@ parser.add_argument("--ip", type=str, default = "localhost:8214", help="IP addre
 parser.add_argument("--device", type=str, default = "cpu", help="Device to run the client on")
 parser.add_argument('--ca', type = str, default= 'ca.pem', help= 'path to CA certificate')
 parser.add_argument('--encryption', type = int, default= 0, help= '1 enables ssl encryption')
+parser.add_argument('--wait_time', type = int, default= 30, help= 'time to wait before sending the next request')
 
 args = parser.parse_args()
 
 configs = {
     "ip_address": args.ip,
-    "wait_time": 10,
+    "wait_time": args.wait_time,
     "device": args.device,
     "encryption": args.encryption,
     "ca": args.ca,
